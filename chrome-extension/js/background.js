c072e51a7faa8d19c8722e2682a25bcc73e48e86
VirajSinghChhabra <153202728+VirajSinghChhabra@users.noreply.github.com>
2024-09-22 02:22:10 +0530

    Modified version of backrgound js. Corrected two separate chrome.runtime functions, combined into one

--- chrome-extension/js/background.js
+++ chrome-extension/js/background.js
@@ -1,17 +1,18 @@
 // To ensure communication between content.js and popup.js
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    // Handle video playing status messages
     if (message.videoPlaying !== undefined) {
+        // Forward the message to other parts of the extension
         chrome.runtime.sendMessage(message);
     }
-});
 
-chrome.runtime.onMessage.addListener((message, sender) => {
+    // Handle video actions
     if (message.action === 'videoAction') {
         // Broadcast to all connected tabs except the sender tab
         chrome.tabs.query({}, (tabs) => {
             tabs.forEach((tab) => {
                 if (tab.id !== sender.tab.id) {
-                chrome.tabs.sendMessage(tab.id, message);
+                    chrome.tabs.sendMessage(tab.id, message);
                 }
             });
         });
